3a38fb373b3dd286455810af23c43e372c8b2038
GeorgeH <georgeharland12@gmail.com>
2024-03-09 18:06:59 +0000

    more terrain types + places of interest seperate

--- src/generationFunctions/generateNewGame.ts
+++ src/generationFunctions/generateNewGame.ts
@@ -5,7 +5,7 @@ import { generateTavern } from "./generateTavern";
 // Accepts optional player name arguments
 
 export const generateNewGame = (playerFirstName: string = "Tom", playerLastName: string = "Karnos"): GameStateType => {
-  const worldSize = 50;
+  const worldSize = 65;
   const taverns: TileType[] = [];
   const npcs: NpcType[] = [];
   const worldGrid: TileType[][] = [[], []];
@@ -13,21 +13,28 @@ export const generateNewGame = (playerFirstName: string = "Tom", playerLastName:
   // generate world grid
   for (let i = 0; i < worldSize; i++) {
     worldGrid[i] = [];
-    let previousXTile = 'forest';
-    let previousYTile = 'forest';
+    let previousXTile = 'unique';
+    let previousYTile = 'unique';
     for (let j = 0; j < worldSize; j++) {
       const randomNumber = Math.random();
       if(j > 0) previousYTile = worldGrid[i][j-1].locationType;
       let matchPreviousRandomFactor = Math.random();
       if (previousXTile === previousYTile) matchPreviousRandomFactor += 0.6;
-      let tileType = 'forest'
+      let tileType = 'unique'
 
-      if (matchPreviousRandomFactor > 0.8) tileType = Math.random() > 0.5 ? previousXTile : previousYTile;
-      else if (randomNumber > 0.66) tileType = "plains";
-      else if (randomNumber > 0.33) tileType = "forest";
-      else if (randomNumber > 0.01) tileType = "mountain";
-      else if (randomNumber > 0.005) tileType = "ruins";
-      else tileType = "tavern";
+      if (matchPreviousRandomFactor > 0.85 && previousXTile != 'unique' && previousYTile != 'unique') tileType = Math.random() > 0.5 ? previousXTile : previousYTile;
+      else if (randomNumber > 0.75) tileType = "forest"; // x2 chance
+      else if (randomNumber > 0.625) tileType = "plains";
+      else if (randomNumber > 0.5) tileType = "hilllands";
+      else if (randomNumber > 0.375) tileType = "mountain";
+      else if (randomNumber > 0.25) tileType = "desert";
+      else if (randomNumber > 0.125) tileType = "tundra";
+      else tileType = "swamp";
+
+      const placeOfInterest = Math.random() > 0.98;
+      if(placeOfInterest) {
+        tileType = (Math.random() > 0.5) ? "ruins" : "tavern";
+      }
 
       switch(tileType) {
         case 'plains':
@@ -57,6 +64,42 @@ export const generateNewGame = (playerFirstName: string = "Tom", playerLastName:
             y: j,
           };
           break;
+          case 'swamp':
+            worldGrid[i][j] = {
+              id: Math.floor(Math.random() * 1000000),
+              name: "Swamp",
+              locationType: "swamp",
+              x: i,
+              y: j,
+            };
+            break;
+            case 'hilllands':
+          worldGrid[i][j] = {
+            id: Math.floor(Math.random() * 1000000),
+            name: "Hilllands",
+            locationType: "hilllands",
+            x: i,
+            y: j,
+          };
+          break;
+          case 'desert':
+          worldGrid[i][j] = {
+            id: Math.floor(Math.random() * 1000000),
+            name: "Desert",
+            locationType: "desert",
+            x: i,
+            y: j,
+          };
+          break;
+          case 'tundra':
+          worldGrid[i][j] = {
+            id: Math.floor(Math.random() * 1000000),
+            name: "Tundra",
+            locationType: "tundra",
+            x: i,
+            y: j,
+          };
+          break;
         case 'ruins':
           worldGrid[i][j] = {
             id: Math.floor(Math.random() * 1000000),
@@ -79,7 +122,8 @@ export const generateNewGame = (playerFirstName: string = "Tom", playerLastName:
             y: j,
           };
       }
-      previousXTile = tileType;
+      if(tileType === ('tavern' || 'ruins')) previousXTile = 'unique'
+      else previousXTile = tileType
     }
   }
 
@@ -143,15 +187,27 @@ export const generateNewGame = (playerFirstName: string = "Tom", playerLastName:
         case 'forest':
           rowString += "f";
           break;
+        case 'hilllands':
+          rowString += "h";
+          break;
         case 'mountain':
           rowString += "m"
           break;
+        case 'desert':
+          rowString += "d";
+          break;
+        case 'tundra':
+          rowString += "t";
+          break;
+        case 'swamp':
+          rowString += "s";
+          break;
         case 'ruins':
-          rowString += "r"
+          rowString += "R"
           break;
         case 'tavern':
           const isStartingTavern = tile.x === startingTavern.x && tile.y === startingTavern.y;
-          rowString += isStartingTavern ? "T" : "t";
+          rowString += isStartingTavern ? "*T*" : "T";
           break;
         default:
           rowString += "?";
